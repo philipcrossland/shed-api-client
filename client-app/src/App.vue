bef7c828b64206b6c99df644f6812ff2fbae8a7b
Phil Crossland <phil@thirdwheelstudios.com>
2021-03-20 18:07:39 +0000

    Initial header styling and motion vue

--- client-app/src/App.vue
+++ client-app/src/App.vue
@@ -1,32 +1,56 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <div>Shed API</div>
+      <router-link to="/">Motion</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <div class="content">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+  padding: 1rem;
+  background-color: #2980b9;
+  color: #d5e9f6;
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #c0def1;
+    text-decoration: none;
 
-    &.router-link-exact-active {
-      color: #42b983;
+    &.router-link-exact-active,
+    &:hover {
+      color: #eaf4fa;
+      text-decoration: underline;
     }
   }
+
+  > div {
+    display: inline;
+    font-size: 1.5rem;
+    font-weight: 700;
+    text-transform: uppercase;
+    margin-right: 2rem;
+    color: #d5e9f6;
+  }
+}
+
+.content {
+  margin: 1.5rem;
 }
 </style>
