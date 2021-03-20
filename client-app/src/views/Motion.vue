a773e28a60df4a275649f070693e9a7cd439c0b4
Phil Crossland <phil@thirdwheelstudios.com>
2021-03-20 22:11:58 +0000

    Added v. basic table

--- client-app/src/views/Motion.vue
+++ client-app/src/views/Motion.vue
@@ -1,13 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Shed API Client App"/>
-  </div>
+<div>
+  <h2>Motion Sensors</h2>
+  <table>
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Motion Detected</th>
+        <th>Sensor</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>20/03/2021 12:34:21</td>
+        <td>Shed - Indoor</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Component({
   components: {
@@ -16,3 +31,27 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 })
 export default class Motion extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+td,
+th {
+  padding: 0.75rem;
+  vertical-align: top;
+  text-align: left;
+  border-top: 1px solid #dee2e6;
+}
+
+thead th {
+  vertical-align: bottom;
+  border-bottom: 2px solid #dee2e6;
+}
+
+tr :first-child {
+  text-align: center;
+}
+</style>>
