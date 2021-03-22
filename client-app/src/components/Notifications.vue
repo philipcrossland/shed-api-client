b0830bbb71c96962e29100b579f651bc6936ab6e
Phil Crossland <phil@thirdwheelstudios.com>
2021-03-22 08:44:48 +0000

    Added notification bell

--- client-app/src/components/Notifications.vue
+++ client-app/src/components/Notifications.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>🔔</div>
+  <button><font-awesome-icon :icon="['far', 'bell']" /></button>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBell } from "@fortawesome/free-regular-svg-icons";
 
-@Component
+library.add(faBell);
+
+@Component({
+  components: {
+    "font-awesome-icon": FontAwesomeIcon,
+  },
+})
 export default class Notifications extends Vue {
   @Prop() private msg!: string;
 }
@@ -13,4 +22,14 @@ export default class Notifications extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+button {
+  font-size: 1.5rem;
+  background-color: transparent;
+  border: none;
+  color: rgba(#fff, 0.4);
+}
+
+button:focus {
+  outline: none;
+}
 </style>
