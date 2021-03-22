65536e8f52b346f2fc7e6e92797467ef912ca827
Phil Crossland <phil@thirdwheelstudios.com>
2021-03-22 20:57:28 +0000

    The bell lights up and shakes when motion detected

--- client-app/src/components/Notifications.vue
+++ client-app/src/components/Notifications.vue
@@ -1,5 +1,11 @@
 <template>
-  <button><font-awesome-icon :icon="['far', 'bell']" /></button>
+  <button
+    @click="hasUnreadNotifications = !hasUnreadNotifications"
+    v-bind:class="{ 'has-unread-notifications': hasUnreadNotifications }"
+  >
+    <font-awesome-icon v-if="!hasUnreadNotifications" :icon="['far', 'bell']" />
+    <font-awesome-icon v-if="hasUnreadNotifications" :icon="['fas', 'bell']" />
+  </button>
 </template>
 
 <script lang="ts">
@@ -7,16 +13,35 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faBell } from "@fortawesome/free-regular-svg-icons";
+import { faBell as faSolidBell } from "@fortawesome/free-solid-svg-icons";
+import * as signalR from "@microsoft/signalr";
 
-library.add(faBell);
+library.add(faBell, faSolidBell);
 
 @Component({
   components: {
     "font-awesome-icon": FontAwesomeIcon,
   },
+  data: () => ({
+    hasUnreadNotifications: false,
+  }),
 })
 export default class Notifications extends Vue {
-  @Prop() private msg!: string;
+  hasUnreadNotifications!: boolean;
+
+  mounted(): void {
+    const baseUrl: string = process.env.VUE_APP_API_BASE_URL as string;
+    const hubUrl = `${baseUrl}/realtimehub`;
+
+    let connection = new signalR.HubConnectionBuilder().withUrl(hubUrl).build();
+
+    connection.on("motionsensed", (sensorName) => {
+      this.hasUnreadNotifications = true;
+      console.log(`Motion Sensed: ${sensorName}`);
+    });
+
+    connection.start().then(() => console.log("SignalR connection started"));
+  }
 }
 </script>
 
@@ -36,5 +61,26 @@ button:focus {
 
 button.has-unread-notifications {
   color: rgba(#fff, 0.8);
+  transform: translate3d(0, 0, 0);
+  animation: shake 1s;
+  cursor: pointer;
+}
+
+@keyframes shake {
+  20%,
+  80% {
+    transform: translate3d(1px, 0, 0);
+  }
+
+  30%,
+  50%,
+  70% {
+    transform: translate3d(-3px, 0, 0);
+  }
+
+  40%,
+  60% {
+    transform: translate3d(3px, 0, 0);
+  }
 }
 </style>
